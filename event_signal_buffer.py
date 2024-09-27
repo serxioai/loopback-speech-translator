619fdf6c4a6ad7392f7d318c6711407abfc6cc8a
Seth Hammock <sethhammock@gmail.com>
2024-09-27 10:15:51 -0500

    Updating event signal refactor

--- event_signal_buffer.py
+++ event_signal_buffer.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import difflib
 
 class Observer(ABC):
     @abstractmethod
@@ -9,8 +10,13 @@ class EventSignalBuffer:
     def __init__(self):
         self.buffer = {}
         self._observers = []
-        
-        self.recognizing_event_counter = 0
+
+        # Temporary storage for the recognizing text output 
+        self.observable_buffer = {}
+        self.comparison_buffer = {}
+
+        # Add a pointer dictionary to track the current item being processed/displayed for each language
+        self.current_pointer = {}
         
     def init_buffer(self, languages):
             # Dictionary to hold the result from the recognized events
@@ -19,10 +25,9 @@ class EventSignalBuffer:
     def update(self, translations, reason):
         if reason == "RECOGNIZING":
             for language, translation in translations.items():
-                
-                if language in self.buffer:
-                    self.buffer[language].append(translation)
-                    print("RECOGNIZING BUFFER:", self.buffer)
+                self.synthesize_recognizing_translation(language, translation)
+                self.buffer[language].append(self.get_next_translation(language))
+                print("RECOGNIZING BUFFER:", self.buffer)
         elif reason == "RECOGNIZED":
             for language, translation in translations.items():
                 if language in self.buffer:
@@ -37,3 +42,49 @@ class EventSignalBuffer:
     def _notify(self):
         for observer in self._observers:
             observer.update(self.buffer)
+
+    def synthesize_recognizing_translation(self, language_code, translation):
+         # Check if language_code exists in the buffers
+        if language_code not in self.observable_buffer:
+            self.observable_buffer[language_code] = []
+        if language_code not in self.comparison_buffer:
+            self.comparison_buffer[language_code] = ''
+        
+        d = difflib.Differ()
+        diff = list(d.compare(self.comparison_buffer[language_code], translation))
+        temp_observable = []
+        addition = False
+
+        for line in diff:
+            # Check if it's an addition
+            if line.startswith('+ '):
+                addition = True
+                temp_observable.append('+')
+                temp_observable.append(line[2:])
+            # Check if it's a common element (neither addition nor deletion)
+            elif line.startswith('  '):
+                if addition:
+                    # Add a space before the next word if the previous one was an addition
+                    temp_observable.append(' ')
+                temp_observable.append(line[2:])
+                addition = False
+            # For deletions, skip
+            else:
+                continue
+
+        # Append the observable for the language in the observable_buffer to maintain backlog
+        self.observable_buffer[language_code].append(''.join(temp_observable))
+
+        # If this language code is not in current_pointer, initialize it
+        if language_code not in self.current_pointer:
+            self.current_pointer[language_code] = 0
+
+        # Update the comparison_buffer for the language
+        self.comparison_buffer[language_code] = translation    
+
+    def get_next_translation(self, language_code):
+        if language_code in self.observable_buffer and self.current_pointer[language_code] < len(self.observable_buffer[language_code]):
+            transcription = self.observable_buffer[language_code][self.current_pointer[language_code]]
+            self.current_pointer[language_code] += 1
+            return transcription
+        return None    
